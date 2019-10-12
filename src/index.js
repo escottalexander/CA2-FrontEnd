c28d01d55ae8b1051b368f829e5ba93fecdf195a
Camilla <camilla.valerius@gmail.com>
2019-10-12 16:41:34 +0200

    removal of template code and files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import jokes from "./jokes";
 
-const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
-document.getElementById("jokes").innerHTML = allJokes.join("");
 
 
 
