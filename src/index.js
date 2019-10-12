a68ad08978111b5c3e6d536ceedc9dcc03cbab89
Camilla <camilla.valerius@gmail.com>
2019-10-12 16:51:00 +0200

    errorhandling function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 const url = 'https://maltemagnussen.com/CA2/api/search/';
 
+function handleHttpErrors(res) {
+    if (!res.ok) {
+        return Promise.reject({ status: res.status, fullError: res.json() })
+    }
+    return res.json();
+}
+
 
 
