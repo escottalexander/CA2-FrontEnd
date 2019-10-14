709a11b3dca60a1df4ed6e44717b6834cdf019db
Camilla <camilla.valerius@gmail.com>
2019-10-14 11:14:35 +0200

    button

--- src/index.js
+++ src/index.js
@@ -16,10 +16,34 @@ function fillViewPersonWithDataDiv() {
     inputtag.classList.add('ViewPersonWithDataInputTAG');
     inputtag.setAttribute('type', 'text');
     inputtag.setAttribute('placeholder', 'UserName');
+    let buttontag = document.createElement('button');
+    inputtag.classList.add('ViewPersonWithDataButtonTAG');
     let div = document.getElementById("viewPersonWithData");
     div.innerHTML = "";
     div.appendChild(inputtag);
+    div.appendChild(buttontag);
     div.appendChild(ptag);
 }
 
+function singleuser() {
+    let id = document.getElementById('useridinput').value;
+    if(!id){
+        document.getElementById('singleuser').innerHTML = 'Type in an ID'
+    }
+    else{
+        let urlID = 'http://localhost:3333/api/users/' + id;
 
+    fetch(urlID)
+        .then(handleHttpErrors)
+        .then(jsondata => {
+            getUserToEdit(jsondata);
+            document.getElementById('singleuser').innerHTML = JSON.stringify(jsondata);
+        })
+        .catch(err => {
+            if (err.status) {
+                err.fullError.then(e => console.log(e.detail))
+            }
+            else { console.log("Network error"); }
+        });
+    }
+}
