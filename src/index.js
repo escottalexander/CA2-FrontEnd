1b0a254bb4238adc6cc6f7d825af287d8b64a585
Camilla <camilla.valerius@gmail.com>
2019-10-14 13:58:25 +0200

    changed class to id

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css'
 document.addEventListener("DOMContentLoaded", function () {
     fillViewPersonWithDataDiv();
+    // document.getElementById("viewPersonWithDataButtonTAG").addEventListener('click', function (event) {
+    //     event.preventDefault();
+    //     singleuser();
+    // });
 });
 
 const url = 'https://maltemagnussen.com/CA2/api/search/';
@@ -14,14 +18,14 @@ function handleHttpErrors(res) {
 
 function fillViewPersonWithDataDiv() {
     let ptag = document.createElement('p');
-    ptag.classList.add('viewPersonWithDataPTAG');
+    ptag.setAttribute('id', 'viewPersonWithDataPTAG');
     let inputtag = document.createElement('input');
-    inputtag.classList.add('viewPersonWithDataInputTAG');
+    inputtag.setAttribute('id', 'viewPersonWithDataInputTAG');
     inputtag.setAttribute('type', 'text');
     inputtag.setAttribute('placeholder', 'UserName');
     let buttontag = document.createElement('button');
     buttontag.innerHTML = 'Get User';
-    buttontag.classList.add('viewPersonWithDataButtonTAG');
+    buttontag.setAttribute('id', 'viewPersonWithDataButtonTAG');
     let div = document.getElementById("viewPersonWithData");
     div.innerHTML = "";
     div.appendChild(inputtag);
@@ -29,30 +33,36 @@ function fillViewPersonWithDataDiv() {
     div.appendChild(ptag);
 }
 
+function emptyViewPersonWithDataDiv() {
+    let div = document.getElementById("viewPersonWithData");
+    div.innerHTML = "";
+}
+
 document.getElementById("viewPersonWithDataButtonTAG").addEventListener('click', function (event) {
-    event.preventDefault();
-    singleuser();
+  event.preventDefault();
+   singleuser();
 });
 
 function singleuser() {
+    alert(hat);
     let username = document.getElementById('viewPersonWithDataInputTAG').value;
-    if(!username){
+    if (!username) {
         document.getElementById('viewPersonWithDataInputTAG').innerHTML = 'Type in a name'
     }
-    else{
+    else {
         let urlName = url + '/person/' + username;
 
-    fetch(urlName)
-        .then(handleHttpErrors)
-        .then(jsondata => {
-            getUserToEdit(jsondata);
-            document.getElementById('ViewPersonWithDataPTAG').innerHTML = JSON.stringify(jsondata);
-        })
-        .catch(err => {
-            if (err.status) {
-                err.fullError.then(e => console.log(e.detail))
-            }
-            else { console.log("Network error"); }
-        });
+        fetch(urlName)
+            .then(handleHttpErrors)
+            .then(jsondata => {
+                getUserToEdit(jsondata);
+                document.getElementById('ViewPersonWithDataPTAG').innerHTML = JSON.stringify(jsondata);
+            })
+            .catch(err => {
+                if (err.status) {
+                    err.fullError.then(e => console.log(e.detail))
+                }
+                else { console.log("Network error"); }
+            });
     }
 }
