bb24959d76b3371ee5b06af89655631609ef22c5
HrBjarup <cph-ab363@cphbusiness.dk>
2019-10-15 17:53:15 +0200

    Half way done

--- src/index.js
+++ src/index.js
@@ -69,7 +69,108 @@ function createPersonOptions(){
 /*------------- Begin  Create All -------------*/
 /*---------------------------------------------*/
 
+var outputCreateAll = document.getElementById("outputCreateAll");
 
+var buttonCreateAll = document.getElementById("createAll");
+
+var hobbyName = document.getElementById("inputHobbyNameCreateAll");
+var hobbyDescription = document.getElementById("inputHobbyDescriptionCreateAll");
+var hobbyStatus = document.getElementById("hobbyStatus");
+
+var zipCode = document.getElementById("inputCityZipCreateAll");
+var cityName = document.getElementById("inputCityNameCreateAll");
+var cityStatus = document.getElementById("cityStatus");
+
+
+
+hobbyName.addEventListener("input", function(){
+    checkIfInputExists(false);
+})
+
+zipCode.addEventListener("input", function(){
+    checkIfInputExists(true);
+})
+
+var lastExisted = false;
+function checkIfInputExists(isCity) {
+    //Variables based on boolean isCity
+    //If isCity then the function is being used to check if a city exists
+    //If not then the function is being used to check if a hobby exists
+    var checkValue;
+    var target;
+    var status;
+    var uriPart;
+    if (isCity) {
+        checkValue = zipCode;
+        target = cityName;
+        status = cityStatus;
+        uriPart = "city/zip/";
+    } else {
+        checkValue = hobbyName;
+        target = hobbyDescription;
+        status = hobbyStatus;
+        uriPart = "hobby/";
+    }
+
+    //End of variables
+
+    if (checkValue.value == null || checkValue.value === "") {
+        target.innerText = "";
+        target.value = "";
+        if (!target.hasAttribute("disabled"))
+        {
+            target.setAttribute("disabled", "true");
+        }
+        return;
+    }
+    fetch(testurl + uriPart + checkValue.value)
+    .then(res => handleHttpErrors(res))
+    .then(function(data) {
+        console.log(data);
+        let output;
+        if (isCity)
+        {
+            output = data.city;
+        } else {
+            output = data.description;
+        }
+        if (data.name != null || data.city)
+        {
+            target.innerText = "";
+            target.value = "";
+            status.innerHTML = "-- Existing ✓ --";
+            target.innerText = output;
+            target.value = output;
+            lastExisted = true;
+            if (!target.hasAttribute("disabled"))
+            {
+                target.setAttribute("disabled", "true");
+            }
+        }
+    })
+    .catch(err => {
+        if(err.status){
+            //If we end up here it means that no hobby/city with the given name/zipcode was found
+            if (target.hasAttribute("disabled"))
+            {
+                target.removeAttribute("disabled");
+            }
+            if (lastExisted) 
+            {
+                target.innerText = "";
+                target.value = "";
+                status.innerHTML = "-- New --";
+                lastExisted = false;
+            }
+        }
+        else{console.log("Network error");
+        }
+    });
+}
+
+buttonCreateAll.addEventListener("click", function(){
+    outputCreateAll.innerHTML = hobbyDescription.value;
+})
 
 
 /*---------------------------------------------*/
