d29fbdbb96225915c280283ab3bb6199e60bfeef
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-17 17:50:01 +0200

    Moved Add Person

--- src/index.js
+++ src/index.js
@@ -79,9 +79,11 @@ function navigate() {
     // Set the content div innerHTML based on the fragment identifier.
     getContent(fragment, function (content) {
         contentDiv.innerHTML = content;
+        // ADD YOUR WRAP FUNCTIONS TO THIS SWITCH LIKE SO;
+        // case "nameOfFragment": nameOfFunction(); break; 
         switch (fragment) {
             case "get": get(); break;
-
+            case "add": addPerson(); break;
         }
     });
     changeActiveNavbarElement();
@@ -208,50 +210,51 @@ function emptyTag(divID) {
 
 var output = document.getElementById("output");
 
-var buttonAddSimple = document.getElementById("createSimplePerson");
+function addPerson() {
+    var buttonAddSimple = document.getElementById("createSimplePerson");
 
-buttonAddSimple.addEventListener("click", function () {
-    fetch(url + "create/person", createPersonOptions())
-        .then(res => handleHttpErrors(res))
-        .then(function (data) {
-            console.log(data);
-            output.innerHTML = "<p>Person created:</p><br>"
-                + "<p>ID: " + data.id + "<br>"
-                + "<p>First name: " + data.firstName + "<br>"
-                + "<p>Last name: " + data.lastName + "<br>"
-                + "<p>email: " + data.email + "<br>";
-        })
-        .catch(err => {
-            if (err.status) {
-                err.fullError.then(e => output.innerHTML = "Error:<br><br>")
-            }
-            else { console.log("Network error"); }
-        });
-})
+    buttonAddSimple.addEventListener("click", function () {
+        fetch(url + "create/person", createPersonOptions())
+            .then(res => handleHttpErrors(res))
+            .then(function (data) {
+                console.log(data);
+                output.innerHTML = "<p>Person created:</p><br>"
+                    + "<p>ID: " + data.id + "<br>"
+                    + "<p>First name: " + data.firstName + "<br>"
+                    + "<p>Last name: " + data.lastName + "<br>"
+                    + "<p>email: " + data.email + "<br>";
+            })
+            .catch(err => {
+                if (err.status) {
+                    err.fullError.then(e => output.innerHTML = "Error:<br><br>")
+                }
+                else { console.log("Network error"); }
+            });
+    })
 
-function createPersonOptions() {
-    var FirstName = document.getElementById("inputFirstName").value;
-    var LastName = document.getElementById("inputLastName").value;
-    var Email = document.getElementById("inputEmail").value;
-    var Method = "POST";
-    var data = {
-        firstName: FirstName,
-        lastName: LastName,
-        email: Email
+    function createPersonOptions() {
+        var FirstName = document.getElementById("inputFirstName").value;
+        var LastName = document.getElementById("inputLastName").value;
+        var Email = document.getElementById("inputEmail").value;
+        var Method = "POST";
+        var data = {
+            firstName: FirstName,
+            lastName: LastName,
+            email: Email
+        }
+
+        let options = {
+            method: Method,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+        console.log(options);
+        return options;
     }
-
-    let options = {
-        method: Method,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }
-    console.log(options);
-    return options;
 }
-
 /*---------------------------------------------*/
 /*----------- End Add Person Simple -----------*/
 /*---------------------------------------------*/
